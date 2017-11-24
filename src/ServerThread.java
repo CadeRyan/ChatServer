33f13cb2b9924499cd8f14a8bcd056e078a3ecd5
Cade Ryan <ryanc45@ITSERV.SCSS.TCD.IE>
2017-11-24 18:45:14 +0000

    hey we're actually getting somewhere!

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -2,6 +2,7 @@ import java.net.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,9 +33,10 @@ public class ServerThread extends Thread{
 			String message = "";
 			boolean notification = false;
 			Chatroom currentChatroom = null;
+			boolean leave = false;
 
 			while ((inputLine = in.readLine()) != null) {
-				
+
 				if(inputLine.contains("KILL_SERVICE\n")){
 					break;
 				}
@@ -45,10 +47,14 @@ public class ServerThread extends Thread{
 					outputLine += "Port:1234\n";
 					outputLine += "StudentID:14310841\n";
 				}
+				//_________________________________________________________________
+				// here lies the logic for joining a chatroom
+
 				else if(inputLine.contains("JOIN_CHATROOM: ") && state == 0){
 
 					String chatroomName = actualData(inputLine);
 					String roomRef = "";
+					//System.out.println(chatroomName);
 					chtrmNumber = chatroomExists(chatroomName);
 					if(chtrmNumber > 0)
 						roomRef = "" + chtrmNumber;
@@ -73,40 +79,85 @@ public class ServerThread extends Thread{
 					int clntExists = clientExists(clientName);
 					int clientJoinID = clntExists;
 					if(clntExists > 0){
+						System.out.println(joinID);
 						joinID = "" + clntExists;
 					}
 					else{
-						Client new1 = new Client();
-						new1.name = clientName;
-						new1.socket = socket;
-						Server.allClients.put(new1.joinID, new1);
-						clientJoinID = new1.joinID;
-						joinID = "" + new1.joinID;
+						//Client new1 = new Client();
+						thisClient.name = clientName;
+						//System.out.println(thisClient.name);
+						thisClient.socket = socket;
+						Server.allClients.put(thisClient.joinID, thisClient);
+						clientJoinID = thisClient.joinID;
+						joinID = "" + thisClient.joinID;
 					}
 					Chatroom tmp = Server.chatrooms.get(chtrmNumber);
 					thisClient = Server.allClients.get(clientJoinID); 
+					
 					if(!(clientIsInChatroomAlready(tmp, thisClient))){
 						tmp.addClientToChatroom(thisClient);
 					}
 					outputLine += "JOIN_ID: " + joinID + "\n";
 					notification = true;
 					currentChatroom = tmp;
-					message = thisClient.name + " has just joined the chatroom";
+					message = "CHAT: " + currentChatroom.roomRef + "\n"
+							+ "CLIENT_NAME: " + thisClient.name + "\n"
+							+ "MESSAGE: " + thisClient.name + " has just joined the chatroom\n\n";
 					state = 0;
 				}
 
+				//___________________________________________________________________
+				// here lies the logic for leaving the chatroom
+
+				else if(inputLine.contains("LEAVE_CHATROOM: ")&& state == 0){
+
+					int roomReference = Integer.parseInt(actualData(inputLine));
+					currentChatroom = Server.chatrooms.get(roomReference);
+					outputLine = "LEFT_CHATROOM: " + roomReference + "\n";
+					state = 4;
+				}
+				else if(inputLine.contains("JOIN_ID: ")&& state == 4){
+					int joinID = Integer.parseInt(actualData(inputLine));
+					thisClient = Server.allClients.get(joinID);
+					outputLine += inputLine + "\n";
+					leave = true;
+					state = 5;
+				}
+				else if(inputLine.contains("CLIENT_NAME: ")&& state == 5){
+					notification = true;
+					message = "CHAT: " + currentChatroom.roomRef + "\n"
+							+ "CLIENT_NAME: " + thisClient.name + "\n"
+							+ "MESSAGE: " + thisClient.name + " has just left the chatroom\n\n";
+					state = 0;
+				}
+
+				//___________________________________________________________________
 				if(outputLine != null && state == 0) {
 					out.write(outputLine.getBytes());
 					if(notification && currentChatroom != null){
 						notifyOtherClients(currentChatroom.clients, thisClient, message);
+						if(leave){
+							removeClientFromChatroom(currentChatroom, thisClient);
+						}
 						notification = false;
+						leave = false;
 						currentChatroom = null;
+						message = "";
 					}
+					outputLine = "this should not be showing";
 				}
 			}
 			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	public void removeClientFromChatroom(Chatroom a, Client b){
+
+		for(int i = 0; i < a.clients.size(); i++){
+			if(a.clients.get(i).joinID == b.joinID){
+				a.clients.remove(i);
+			}
 		}
 	}
 	public String actualData(String a){
@@ -129,37 +180,35 @@ public class ServerThread extends Thread{
 		return res;
 	}
 	public void notifyOtherClients(ArrayList<Client> clientsInChatroom, Client b, String message){
-		
+
 		for(int i = 0; i < clientsInChatroom.size(); i ++){
-			//if(clientsInChatroom.get(i) != b){
-				try {
-					DataOutputStream out = new DataOutputStream(clientsInChatroom.get(i).socket.getOutputStream());
-					out.write(message.getBytes());
-				} catch (IOException e) {
-					e.printStackTrace();
+			try {
+				if(clientsInChatroom.size()>1){
+					System.out.println(clientsInChatroom.get(i).name);
 				}
-			//}
+				DataOutputStream out = new DataOutputStream(clientsInChatroom.get(i).socket.getOutputStream());
+				out.write(message.getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	public int chatroomExists(String a){
 		int res = 0;
-		Set set = Server.chatrooms.entrySet();
-		Iterator iterator = set.iterator();
-		while(iterator.hasNext()){
-			Map.Entry mentry = (Map.Entry)iterator.next();
-			if(a.equalsIgnoreCase((String) mentry.getValue()))
-				res = (int) mentry.getKey();
+		Map<Integer, Chatroom> map = Server.chatrooms;
+		for(Map.Entry<Integer, Chatroom> entry : map.entrySet()){
+			if(a.equalsIgnoreCase(entry.getValue().name)){
+				res = (int) entry.getKey();
+			}
 		}
 		return res;
 	}
 	public int clientExists(String a){
 		int res = 0;
-		Set set = Server.allClients.entrySet();
-		Iterator iterator = set.iterator();
-		while(iterator.hasNext()){
-			Map.Entry mentry = (Map.Entry)iterator.next();
-			if(a.equalsIgnoreCase((String) mentry.getValue()))
-				res = (int) mentry.getKey();
+		Map<Integer, Client> map = Server.allClients;
+		for(Map.Entry<Integer, Client> entry : map.entrySet()){
+			if(a.equalsIgnoreCase(entry.getValue().name))
+				res = (int) entry.getKey();
 		}
 		return res;
 	}
@@ -167,7 +216,6 @@ public class ServerThread extends Thread{
 
 		for(int i = 0; i < a.clients.size(); i ++){
 			if(a.clients.get(i) != null && a.clients.get(i).joinID == b.joinID){
-				System.out.println(i);
 				return true;
 			}
 		}
